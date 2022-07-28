754bfc7e37206d19b3c6828751b337f61482e2b3
Mikael T Jaworski <nameLess@mikaels-mbp.myfiosgateway.com>
2022-07-28 16:47:53 -0400

    fixed merge conflict

---
+++ consoleapps/console5app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console5app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You were my brother Anakin! I loved you.");
+        }
+    }
+}
